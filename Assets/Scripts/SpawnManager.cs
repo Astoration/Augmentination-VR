9996b5e5dcae20f941770bf40c8c5ff0a0bcd5cc
Astoration <ybdh00@naver.com>
2016-10-25 12:11:07 +0900

    add gem item

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SpawnManager : MonoBehaviour {
 	public GameObject[] monsters = new GameObject[5];
+	public GameObject item;
 
 	public float time = 10f;
 	// Use this for initialization
@@ -12,8 +13,16 @@ public class SpawnManager : MonoBehaviour {
 			yield return new WaitForSeconds (time);
 		}
 	}
+	IEnumerator itemSpawn(){
+		while (true) {
+			Instantiate (item, new Vector3 (10f * (Random.Range (0, 2) == 0 ? -1 : 1), 1.37f, Random.Range(-95f,95f)), Quaternion.identity);
+			yield return new WaitForSeconds (time*4);
+		}
+	}
 	void Start () {
 		StartCoroutine (spawn ());
+		StartCoroutine (itemSpawn ());
+
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/tree.cs
+++ Assets/Scripts/tree.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 
 public class tree : MonoBehaviour {
-
+	public bool isGem = false;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.forward * 30f * Time.deltaTime);
 		if (transform.position.z > 95f) {
 			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionStay(Collision other){
+		if (other.gameObject.CompareTag ("aim")&&isGem) {
+			if (Input.touchCount > 0) {
+				LifeManager.instance.life += 1;
+				Destroy (this.gameObject);
+			}
 		}
 	}
 }
